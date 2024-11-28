df96b74dc2644496f88eee97b84a713eb4c462e4
aymenestaff <126107820+aymenestaff@users.noreply.github.com>
2024-11-28 17:12:36 -0500

    Created new item service and displayed items

--- angular-app/src/app/app.component.ts
+++ angular-app/src/app/app.component.ts
@@ -1,17 +1,40 @@
-import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ItemService } from './services/item.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'mangodb-angualr';
+  items: any[] = [];
+  newItem = { name: '', description: '' };
+  isLoading : boolean = false;
 
-  constructor() {
-    console.log('AppComponent constructor');
+  constructor(private itemService: ItemService) {}
+
+  ngOnInit(): void {
+    this.loadItems();
   }
+
+  loadItems(): void {
+    this.isLoading = true;
+    this.itemService.getItems().subscribe((data) => {
+      this.items = data;
+      this.isLoading = false;
+    });
+  }
+
+  addItem(): void {
+    this.itemService.addItem(this.newItem).subscribe((item) => {
+      this.items.push(item);
+      this.newItem = { name: '', description: '' };
+    });
+  }
+
 }
---
+++ angular-app/src/app/services/item.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ItemService {
+  private apiUrl = 'http://localhost:5000/items';
+
+  constructor(private http: HttpClient) {}
+
+  getItems(): Observable<any[]> {
+    return this.http.get<any[]>(this.apiUrl);
+  }
+
+  addItem(item: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, item);
+  }
+}
